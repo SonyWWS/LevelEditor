aa69df04161e59f03e6819a55602605ad86911b3
abeckus <alan_beckus@playstation.sony.com>
2015-03-19 12:08:56 -0700

    Changes: - Fixed relative path in GenSchemaDef.bat - Added DomGen.exe tool, for generating Schema.cs file.

--- LevelEditor/schemas/GenSchemaDef.bat
+++ LevelEditor/schemas/GenSchemaDef.bat
@@ -1,1 +1,3 @@
-..\..\..\wws_atf\DevTools\DomGen\bin\DomGen.exe level_editor.xsd Schema.cs "gap" LevelEditor
+@set domgen=..\..\..\wws_atf\DevTools\DomGen\bin\DomGen.exe
+@IF NOT EXIST %domgen% ( set domgen=..\..\ATF\DevTools\DomGen\DomGen.exe)
+%domgen% level_editor.xsd Schema.cs "gap" LevelEditor
